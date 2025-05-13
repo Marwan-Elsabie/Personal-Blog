2cd7ab9a480e7708d43a49250bd8e42d825c22d2
MARWAN ELSABIE <marwan.2000@windowslive.com>
2025-05-13 23:59:20 +0300

    Update dependencies

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -89,11 +89,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url  
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'postgres://blog_user:Ma99906083@localhost:5432/blog_db'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        conn_health_checks=True
+        ssl_require=True
     )
 }
 # Security settings
@@ -168,6 +169,3 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Heroku settings
-import django_heroku
-django_heroku.settings(locals())
